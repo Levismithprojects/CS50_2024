5e1d7fba99fa20f8a07645fce1e0d12e822ec436
levismith123 <levi.smith.applicant@gmail.com>
2024-03-02 20:19:13 +1000

    Updated credit.c

--- credit.c
+++ credit.c
@@ -1,8 +1,34 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+
+
+
+int checksum(long long cardnum){
+	
+	long long data = cardnum;
+	int len = 0;
+
+	while (data != 0){
+		len++;
+		data = data/10;
+	}
+
+	long long data2 = cardnum;
+	int total = 0;
+	for(int i = 0; i < len; i++){
+		if (i %2 != 0){
+			total += (data2%10)*2;
+		}
+
+		data2 = data2/10;
+	}
+
+	return total;
+}
 
 int main()
 {
-	long cardnum = 4003600000000014;
-	cout << cardnum % 10;
+	long long cardnumber = 4003600000000014;
+	printf("%lli\n", cardnumber);
+	printf("%d", checksum(cardnumber));
+	
 }
