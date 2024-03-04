7482887c27c8f382073db69ea8dfbe7967475985
levismith123 <levi.smith.applicant@gmail.com>
2024-03-04 17:06:47 +1000

    Finished exercise
    Successfully checks card type and if it is valid according to Luhn's algorithm

--- credit.c
+++ credit.c
@@ -1,34 +1,106 @@
 #include <stdio.h>
 
 
+int numberlen(long long number){
+	int len = 0;
 
+	while (number != 0){
+		len++;
+		number = number/10;
+	}
+
+	return len;
+}
 int checksum(long long cardnum){
 	
 	long long data = cardnum;
-	int len = 0;
-
-	while (data != 0){
-		len++;
-		data = data/10;
-	}
+	int length = numberlen(cardnum);
 
 	long long data2 = cardnum;
-	int total = 0;
-	for(int i = 0; i < len; i++){
+	int oddtotal = 0;
+	int eventotal = 0;
+
+	for(int i = 0; i < length; i++){
 		if (i %2 != 0){
-			total += (data2%10)*2;
+			int oddresult = (data2%10)*2;
+			
+
+			if ((data2%10*2)/10 != 0){
+				
+				oddtotal += oddresult%10;
+				oddresult /= 10;
+				oddtotal += oddresult;
+				
+			}
+
+			else{
+				oddtotal += oddresult;
+				
+			}
+			
+		}
+
+		else{
+			
+			eventotal += data2%10;
+
 		}
 
 		data2 = data2/10;
 	}
 
-	return total;
+	if((oddtotal + eventotal)%10 == 0){
+		return 0;
+	}
+	else{
+		return 1;
+	}
 }
 
 int main()
 {
-	long long cardnumber = 4003600000000014;
-	printf("%lli\n", cardnumber);
-	printf("%d", checksum(cardnumber));
+	const int mincardlength = 13;
+	const int maxcardlength = 16;
+	long long cardnumber;
+	
+	printf("Input number: \n");
+	scanf("%lld", &cardnumber);
+
+	
+	if(numberlen(cardnumber) >= mincardlength && numberlen(cardnumber) <= maxcardlength)
+		if(checksum(cardnumber) == 0){
+
+			switch(numberlen(cardnumber)){
+			case 15:
+				printf("American Express");
+				break;
+
+			case 13:
+				printf("Visa");
+				break;
+
+			case 16:
+				if(cardnumber%10000000000000000 == 4){
+					printf("Visa");
+				}
+
+				else{
+					printf("Mastercard");
+				}
+				break;
+		}
+		printf("\n");
+		printf("Valid");
+
+		}
+
+		else{
+			printf("Invalid");
+		}
+
+	else{
+		printf("Invalid");
+	}
+	
 	
 }
